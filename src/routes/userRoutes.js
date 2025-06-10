77ec106df2ad0241a5128be09dd7cd88460df01b
ashley763 <meiping.mak@gmail.com>
2025-06-10 21:11:23 +0800

    Update all files

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const userController = require("../controllers/userController");
-const jwtMiddleware=require("../middlewares/jwtMiddleware");
 const router = express.Router();
+const userController = require("../controllers/userController.js");
+const jwtMiddleware = require('../middlewares/jwtMiddleware.js');
 
+router.get("/", userController.getAllUser);
+router.get("/:id", userController.getUserById);
+router.post('/', userController.createNewUser);
+router.put('/:id', userController.updateUserById);
+router.delete('/:id', userController.deleteUserById);
+router.post("/login", userController.loginUser,jwtMiddleware.generateToken,jwtMiddleware.sendToken);
 
 module.exports = router;
